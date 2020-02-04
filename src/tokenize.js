b429baeeb43a326bbfc813b5782e2ab401684200
Michael1142 <michael1142@hotmail.co.uk>
2020-02-04 20:41:45 +0000

    Finished lexer.

--- src/tokenize.js
+++ src/tokenize.js
@@ -28,13 +28,45 @@ const tokenize = input => {
     }
 
     if (isNumber(character)) {
+      let number = character;
+
+      while (isNumber(input[++cursor])) {
+        number += input[cursor];
+      }
       tokens.push({
         type: 'Number',
-        value: parseInt(character),
+        value: parseInt(number),
+      });
+      continue;
+    }
+
+    if (isLetter(character)) {
+      let characters = character;
+
+      while (isLetter(input[++cursor])) {
+        characters += input[cursor];
+      }
+      tokens.push({
+        type: 'Name',
+        value: characters,
+      });
+      continue;
+    }
+
+    if (isQuote(character)) {
+      let string = '';
+      while (isLetter(input[++cursor])) {
+        string += input[cursor];
+      }
+      tokens.push({
+        type: 'String',
+        value: string,
       });
       cursor++;
       continue;
     }
+
+    throw new Error(`${character} is not valid`);
   }
   return tokens;
 };
